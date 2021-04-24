28869837c21738336f1eaf5b65cf1a6f0cbad4d2
MatiasIslaA <matias.isla.a@gmail.com>
2021-04-24 02:10:54 -0400

    Version 2.0

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,7 +1,76 @@
+const attack = document.querySelector(".attack")
+
+document.addEventListener("DOMContentLoaded", () => {
+const protagonist = document.querySelector(".protagonist");
+let bottom = 70;
+let left = 0;
+let isJumping = false;
+
+function jump() {
+    if(isJumping) return;
+    let timerId = setInterval( function() {
+        if (bottom > 100) {
+            clearInterval(timerId)
+            let timerDownId = setInterval( function() {
+                if (bottom <= 70) {
+                    clearInterval(timerDownId);
+                    isJumping = false;
+                }
+                bottom -=5
+                protagonist.style.bottom = bottom + "px"
+            }, 20)
+        }
+        isJumping = true;
+        bottom += 60;
+        protagonist.style.bottom = bottom +"px"}, 20)
+}
+
+function slideLeft() {
+    left -= 25;
+    protagonist.style.left = left +"px";
+
+}
+
+
+
+function slideRight() {
+    if(left < 800){
+        left += 25;
+        protagonist.style.left = left +"px";
+    } else if(left > 200){
+        attack.style.visibility = "visible";
+    }
+}
+
+function control(e) {
+    if (e.keyCode === 38) {
+        jump();
+    } else if(e.keyCode === 39) {
+        slideRight();
+    } else if(e.keyCode === 37) {
+        slideLeft();
+}}
+document.addEventListener("keydown", control)
+
+}
+)
+
+///////////////////////////////
+
+const enemy = document.querySelector(".enemy")
+const popup = document.querySelector(".popup")
+let weapons = document.querySelector(".weapons")
+enemy.addEventListener('click', () => {   
+    attack.style.visibility = "hidden";
+    weapons.style.opacity = "1";
+    popup.style.opacity = "1";
+    
+});
+
 let userScore = 0;
 let computerScore = 0;
-// function that get's a random number between
-// 'min' and 'max'
+// // function that get's a random number between
+// // 'min' and 'max'
 
 function selections(player, robot) {
     if (player == robot) {
@@ -67,13 +136,71 @@ let winner = function cor(a, b) {
     }
 }
 
-// Loop of the game
-let computerNumber = (Math.round(randomNumber(1, 3)))
-for (i=0; i <5; i++) {
-    let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
-    console.log(selections(playerSelection, gameon(computerNumber)));
-    console.log(userScore + " " + computerScore);
-    computerNumber = randomNumber(1, 3);
-}
+let rock = document.querySelector(".rock");
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
+let damagePro = document.querySelector(".damage-protagonist");
+let damageEne = document.querySelector(".damage-enemy");
+let results = document.querySelector(".game-results")
 
-console.log(winner(userScore, computerScore));
+rock.addEventListener('click', () => { 
+    game = selections("rock", gameon(Math.round(randomNumber(1, 3))));
+    document.getElementById("p1").innerHTML = game;
+    if (game == "You win!") {
+        damagePro.classList.remove('show');
+        // this force-restarts the CSS animation
+        void damagePro.offsetWidth;
+        damagePro.classList.add('show');
+    } else if(game == "You lose!") {
+        damageEne.classList.remove('show');
+        // this force-restarts the CSS animation
+        void damageEne.offsetWidth;
+        damageEne.classList.add('show');
+    }
+    document.getElementById("p2").innerHTML = userScore + ' ' + computerScore;
+    if(userScore ==5) {
+        document.querySelector(".weapons").innerHTML = "You have won the game!";
+    } else if(computerScore ==5) 
+        document.querySelector(".weapons").innerHTML = "You have lost the game!";
+})
+paper.addEventListener('click', () => { 
+    game = selections("paper", gameon(Math.round(randomNumber(1, 3))));
+    document.getElementById("p1").innerHTML = game;
+    if (game == "You win!") {
+        damagePro.classList.remove('show');
+        // this force-restarts the CSS animation
+        void damagePro.offsetWidth;
+        damagePro.classList.add('show');
+    } else if(game == "You lose!") {
+        damageEne.classList.remove('show');
+        // this force-restarts the CSS animation
+        void damageEne.offsetWidth;
+        damageEne.classList.add('show');
+    }
+    document.getElementById("p2").innerHTML = userScore + ' ' + computerScore;
+    if(userScore ==5) {
+        document.querySelector(".weapons").innerHTML = "You have won the game!";
+    } else if(computerScore ==5) 
+        document.querySelector(".weapons").innerHTML = "You have lost the game!";
+})
+scissors.addEventListener('click', () => { 
+    game = selections("scissors", gameon(Math.round(randomNumber(1, 3))));
+    document.getElementById("p1").innerHTML = game;
+    if (game == "You win!") {
+        damagePro.classList.remove('show');
+        // this force-restarts the CSS animation
+        void damagePro.offsetWidth;
+        damagePro.classList.add('show');
+    } else if(game == "You lose!") {
+        damageEne.classList.remove('show');
+        // this force-restarts the CSS animation
+        void damageEne.offsetWidth;
+        damageEne.classList.add('show');
+    }
+    document.getElementById("p2").innerHTML = userScore + ' ' + computerScore;
+    if(userScore ==5) {
+        document.querySelector(".weapons").innerHTML = "You have won the game!";
+    } else if(computerScore ==5) 
+        document.querySelector(".weapons").innerHTML = "You have lost the game!";
+
+})
